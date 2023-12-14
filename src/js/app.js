e530dd59bd87610192717dd7f1c2fec42b0a4c36
UlyBala <bekzhankamalov05@gmail.com>
2023-12-14 19:37:30 +0600

    feat: add tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    $('.slider').slick();
+    $('.slider').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./src/icons/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./src/icons/arrow-right.svg" alt=""></button>'
+    });
+
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 });
